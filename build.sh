61d6de5a3d0c37f3269ae8a98ff207b92c53a059
ElmGone <mrcs.elmgone@mailnull.com>
2016-07-05 17:32:39 +0200

    add upx

--- build.sh
+++ build.sh
@@ -9,4 +9,16 @@ if which eg.sh > /dev/null; then
 	EG=$(which eg.sh)
 fi
 
-(cd wui && $EG go generate) && $EG go install -race && coligui wui
+if echo $* | grep upx > /dev/null ; then
+	UPX="$EG upx --ultra-brute /go/bin/coligui"
+else
+	UPX="echo running coligui ..."
+fi
+
+(cd wui && $EG go generate) &&
+	$EG go install -race &&
+	$UPX &&
+	coligui wui
+
+## 	$EG upx --ultra-brute /go/bin/coligui &&
+##
